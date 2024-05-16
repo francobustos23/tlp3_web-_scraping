3086afd4e69f85e32c3e623a85be0cf33e1f3118
francobustos23 <bustosfranco071@gmail.com>
2024-05-15 22:52:13 -0300

    a dictionary was created to save the urls and the results, also the code to save the dictionary in a json file

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
+import json
 
 # URL of the website
-url = 'https://es.wikipedia.org/wiki/Kobe_Bryant'
+url = 'https://www.mercadolibre.com.ar/c/autos-motos-y-otros#menu=categories'
 
 # Get the HTML of the website
 response = requests.get(url)
@@ -11,24 +11,35 @@ response = requests.get(url)
 # Create a BeautifulSoup object with the HTML of the website
 soup = BeautifulSoup(response.text, 'html.parser')
 
-results = soup.find_all('a')
+# Find all relevant links
+results = soup.find_all('a', class_='splinter-link main-slider-item')
 
+# Dictionary to store the data
+data = {}
+
+# Iterate over each link
 for result in results:
-    if 'href' in result.attrs:
-        # Verificar si el enlace tiene una URL completa
-        if result['href'].startswith('http'):
-            full_url = result['href']
-        else:
-            # Unir la URL base con el href del elemento
-            full_url = urljoin(url, result['href'])
-            
-        # Realizar la solicitud solo si es una URL completa
-        response2 = requests.get(full_url)
-        
-        soup2 = BeautifulSoup(response2.text, 'html.parser')
-        
-        results2 = soup2.find_all(['h1','p'])
-        
-        print(results2)
-    else:
-        print("No 'href' attribute found.")
+    # Get the URL of the link
+    a_url = result['href']
+
+    # Get the HTML of the link
+    response2 = requests.get(a_url)
+
+    # Create a BeautifulSoup object with the HTML of the link
+    soup2 = BeautifulSoup(response2.text, 'html.parser')
+    
+    # Find all h1 and p elements
+    results2 = soup2.find_all(['h1', 'p'])
+    
+    # Extract text content from the results2 
+    # and convert it to a string to keep the element labels 
+    extracted_text = [str(element) for element in results2]
+    
+    # Store the URL and the extracted text in the dictionary
+    data[a_url] = extracted_text
+
+# Save the data to a JSON file
+with open('results.json', 'w', encoding='utf-8') as json_file:
+    json.dump(data, json_file, ensure_ascii=False, indent=4)
+
+print("Data has been saved to results.json")
